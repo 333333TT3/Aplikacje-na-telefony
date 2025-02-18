b9927e35099eb01852e011072a4717ca59a38d27
q <q>
2025-02-18 11:13:09 +0100

    ADDED FILES

---
+++ PeopleDatabaseMaui/PeopleDatabaseClassLibrary/PeopleRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleDatabaseClassLibrary
+{
+    public class PeopleRepository
+    {
+        private PeopleDBContext dbContext;
+
+        public PeopleRepository()
+        {
+            dbContext = new PeopleDBContext(); ;
+        }
+    }
+}
